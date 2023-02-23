870a47289e0214184d070f5304984b1fa6c9a014
Austin Friesorger <aafrieso@svsu.edu>
2023-02-23 14:14:26 -0500

    add create function to quiz controller

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,7 +1,16 @@
 const { Quiz } = require('../models')
 const cloudinary = require('cloudinary').v2
 
+const create = async (req, res) => {
+  try {
+    const quiz = await Quiz.create(req.body);
+    res.status(200).json(quiz);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
 
 
-
-module.exports =
+module.exports = {
+  create
+}
