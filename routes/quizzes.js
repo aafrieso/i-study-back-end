463a189fa38f8fd1cd274030d0408b99b365acb1
Austin Friesorger <aafrieso@svsu.edu>
2023-02-23 13:57:22 -0500

    add to quizzes resource

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 const quizzesCtrl = require('../controllers/quizzes.js')
 
+router.post('/', quizzesCtrl.create)
+router.get('/', quizzesCtrl.index)
+router.put('/:id', quizzesCtrl.update)
+router.delete('/:id', quizzesCtrl.delete)
 
 module.exports = router
